5c4ed656ad0ce7789d8032270ce045ad4fc53614
ilyass <ilyasstube80@gmail.com>
2024-04-03 01:02:47 +0000

    Not finished yet

--- src/main/java/com/example/tp3_ilyass/repository/PatientRepository.java
+++ src/main/java/com/example/tp3_ilyass/repository/PatientRepository.java
@@ -4,7 +4,6 @@ import com.example.tp3_ilyass.entities.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
